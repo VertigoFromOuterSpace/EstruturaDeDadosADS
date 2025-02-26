af5e8658308ae433e2f1c7c3a0a9619fafb6e0e9
HNRQ <justhenriquecarvalho@gmail.com>
2025-02-26 08:38:23 -0300

    Add files via upload

---
+++ Vetores/src/negocio/VetorInvertido.java
@@ -0,0 +1,36 @@
+package negocio;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class VetorInvertido {
+
+	public static void main(String[] args) {
+		
+		//Declaração de variáveis
+		BufferedReader leitor = new BufferedReader(new InputStreamReader(System.in));
+		int[] vetorOriginal = new int[5];
+		int[] vetorInvertido = new int[5];
+		
+		// Entrada de dados
+		
+		try {
+			for (int i = 0 ; i < vetorOriginal.length ; i++) {
+				System.out.println("Digite o valor da posicação " + i + " do vetor original: ");
+				vetorOriginal[i] = Integer.parseInt(leitor.readLine());
+				}
+			} catch (Exception erro) {}
+		
+		// Processamento
+		for (int i = 0 ; i < vetorOriginal.length ; i++) {
+			vetorInvertido[vetorOriginal.length - 1 - i] = vetorOriginal[i];
+		}
+		
+		// Saída de dados
+		for (int i = 0 ; i < vetorInvertido.length ; i++) {
+			System.out.println("VetorInvertido[" + i + "] = " + vetorInvertido[i]);
+		}
+		
+	}
+
+}
---
+++ Vetores/src/negocio/Vetores.java
@@ -0,0 +1,28 @@
+package negocio;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class Vetores {
+
+	public static void main(String[] args) {
+		
+		// Declaração de variáveis
+		BufferedReader leitor = new BufferedReader(new InputStreamReader(System.in));
+		
+		int[] vetor = new int[10];
+		
+		// Entrada de dados
+		try {
+			for (int i = 0 ; i < vetor.length ; i++) {
+				System.out.print("Digite o valor da posição " + i + " do vetor: ");
+				vetor[i] = Integer.parseInt(leitor.readLine());
+			}
+		} catch (Exception erro) {}
+		
+		// Saída de dados
+		for (int i = 0 ; i < vetor.length ; i++) {
+			System.out.println("Vetor [" + i +"] = " + vetor[i]);
+		}
+	}
+}
